be593c44bba89663401c839fe12e4562225cf572
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-04-23 07:58:46 -0600

    Componente buscador

--- gifs-app/src/app/gifs/gifs.module.ts
+++ gifs-app/src/app/gifs/gifs.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent //NO ES NECESARIO EXPORTARLO YA QUE LO USAREMOS SOLO DENTRO DEL HOME COMPONENT
+  ],
   imports: [
     CommonModule//PARA USAR PIPES, NGFOR Y MAS..
+  ],
+  exports:[
+    HomePageComponent
   ]
 })
 export class GifsModule { }
