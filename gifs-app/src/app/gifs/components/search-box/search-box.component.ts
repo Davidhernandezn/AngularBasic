a35cd7adbf6a87e7872b961a517d049e509fa311
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-04-24 22:02:29 -0600

    VIEWCHILD PARA OBTENER UNA REFERENCIA LOCAL

--- gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search-box',
@@ -11,5 +11,25 @@ export class SearchBoxComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  //RECUPERAR LO INGRESADO, recibe
+  /** 
+  searchTag(newTag: string){
+    console.log({newTag});
+  }**/
+
+  //FORMA PARA EL VIEWCHILD ES PARA OBTENER UNA REFERENCIA LOCAL VIEWCHILDREN ES UN ARRAY
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({newTag});
+  }
+
+
+
+  //ViewChild
+  //NECESITA UNO O 2 ARGUMENTS = SELECTOR NOMBRE DEL ELEMEMTO HTML
+  @ViewChild('txtTagInput')//#txtTagInput ES EL ID DEL HTML
+  public tagInput!: ElementRef<HTMLInputElement>;//tagInput! INDICA QUE TRAE VALOR = NON NULL  OPERATOR
+
 
 }
